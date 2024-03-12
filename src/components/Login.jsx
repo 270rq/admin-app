2ea0e8539e1638be35f98f43983f321dec0704a0
katya <chuprova@synaptik.ru>
2024-03-12 14:21:18 +0300

    авторизация

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,53 +1,86 @@
 import React from 'react';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import axios from 'axios';
+
+const registrationHandler = async (e, username, password, handleLoginSuccess, handleLoginError) => {
+  try {
+      if (e) {
+          e.preventDefault();
+      }
+
+      const response = await axios.post('http://localhost:3000/api/signIn', {
+          username,
+          password
+      });
+
+      const token = response.data.token;
+
+      console.log('Login successful! Received token:', token);
+      
+      handleLoginSuccess();
+      
+  } catch (error) {
+      console.error('An error occurred:', error);
+      
+      handleLoginError();
+  }
+};
 
 const Login = () => {
-  const registrationHandler = async (e) => {
-    try{
-      e.preventDefault()
-      const data = await
-    }
-    catch(error){}  }
-
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+  const onFinish = (values) => {
+      handleFormSubmit(values.username, values.password);
   };
 
-  return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваше имя!' }]}
-      >
-        <Input />
-      </Form.Item>
+  const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+  };
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваш пароль!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+  const handleFormSubmit = (username, password) => {
+      registrationHandler(null, username, password, handleLoginSuccess, handleLoginError);
+  };
 
+  const handleLoginSuccess = () => {
+    message.success('Вход выполнен успешно!');
+  };
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Вход
-        </Button>
-      </Form.Item>
-    </Form>
-  );
+  const handleLoginError = () => {
+    message.error('Ошибка входа. Пожалуйста, проверьте введенные данные.');
+  };
+
+    return (
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Пожалуйста, введите ваше имя!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Пожалуйста, введите ваш пароль!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Вход
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
 
 export default Login;
