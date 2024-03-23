47f7afa34c79ca410e0c8723067c5f2e4697de85
270rq <chuprova-katya@list.ru>
2024-03-23 20:35:03 +0300

    "Refactored map component: added state for marker deletion, changed form submission handling, added filtering for placemarks, and updated modal for marker deletion"

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Popconfirm, message, Button } from 'antd';
+import { message, Modal } from 'antd';
 import { YMaps, Map, ZoomControl, SearchControl, Placemark, TypeSelector } from '@pbe/react-yandex-maps';
 import FormDisabledDemo from './form';
 import axios from 'axios';
@@ -7,13 +7,11 @@ import axios from 'axios';
 const DemoAreaMap = () => {
   const [placemarks, setPlacemarks] = useState([]);
   const [newPlacemarksCoordinates, setNewPlacemarksCoordinates] = useState([]);
-
- 
-
-  const mapState = {
-    center: [55.751574, 37.573856], // Центр Москвы
-    zoom: 10, // Уровень масштабирования
-  };
+  const [markerToDelete, setMarkerToDelete] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [selectedPeriod, setSelectedPeriod] = useState('');
+  const [selectedLevel, setSelectedLevel] = useState('');
 
   const [hoveredMarker, setHoveredMarker] = useState(null);
 
@@ -22,63 +20,105 @@ const DemoAreaMap = () => {
   };
 
   const handleMapContextMenu = (e) => {
-      const coords = e.get('coords');
-      console.log(newPlacemarksCoordinates);
-      console.log([...newPlacemarksCoordinates, coords]);
-
-      setNewPlacemarksCoordinates([...newPlacemarksCoordinates, coords]);
+    const coords = e.get('coords');
+    setNewPlacemarksCoordinates([...newPlacemarksCoordinates, coords]);
   };
 
-  const handleFormSubmit = (data) => {
-    console.log(data);
-    if( newPlacemarksCoordinates.length <= 0 ){
-      console.log("Лох выбери где")
-    }
-    else {
-      axios
-        .post('http://localhost:3000/api/placemark', {
-          ...data,
-          markers: newPlacemarksCoordinates
-        })
-        .then((response) => {
-          console.log(response.data);
-          setPlacemarks([...placemarks, response.data]);
-    });
-  }}
+  const handleFormSubmit = (formData) => {
+    setSelectedRegion(formData.region);
+    setSelectedPeriod(formData.period);
+    setSelectedLevel(formData.level);
+  };
+
+  const filterPlacemarks = placemarks.filter((placemark) => {
+    return (
+      (!selectedRegion || placemark.region === selectedRegion) &&
+      (!selectedPeriod || placemark.period === selectedPeriod) &&
+      (!selectedLevel || placemark.level === selectedLevel)
+    );
+  });
+
   const changeMarker = (marksData) => {
     setPlacemarks(marksData);
-  }
-const setDeleteMarkerIndex=  (placemarkIndex)=>{
-  setNewPlacemarksCoordinates(newPlacemarksCoordinates.filter((_, index) => index !== placemarkIndex));
-}
-const deleteMarkerBD = ()=>{
-  // сюда надо предупреждение и удаление бд
-}
+  };
+
+  const startDeleteMarker = (placemarkIndex) => {
+    setNewPlacemarksCoordinates(newPlacemarksCoordinates.filter((_, index) => index !== placemarkIndex));
+  };
+
+  const deleteMarkerBD = (placemark) => {
+    setMarkerToDelete(placemark);
+    showModal();
+  };
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    axios
+      .delete(`http://localhost:3000/api/placemark/${markerToDelete.id}`)
+      .then(() => {
+        setPlacemarks(placemarks.filter((placemark) => placemark.id !== markerToDelete.id));
+        message.success('Метка успешно удалена');
+        setMarkerToDelete(null);
+      })
+      .catch((error) => {
+        message.error('Произошла ошибка при удалении метки');
+      });
+    setOpen(false);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const mapState = {
+    center: [55.751574, 37.573856], // Центр Москвы
+    zoom: 10, // Уровень масштабирования
+  };
+
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: 1 }}>
-      <FormDisabledDemo onFormSubmit={handleFormSubmit} onFlowerChange={changeMarker} />
+        <FormDisabledDemo onFormSubmit={handleFormSubmit} onFlowerChange={changeMarker} />
       </div>
       <div style={{ flex: 1, paddingLeft: '20px', position: "relative" }}>
         <YMaps query={{ apikey: "ed158a2d-97a9-49a1-8011-28555c611f7a" }}>
           <Map state={mapState} width="100%" height="500px" options={{ suppressMapOpenBlock: true }} onClick={handleMapContextMenu}>
             <ZoomControl options={{ float: 'right' }} />
             <SearchControl options={{ float: 'left' }} />
-            {placemarks.map((placemark, index) => (
+            {filterPlacemarks.map((placemark, index) => (
               <Placemark
-  key={index}
-  geometry={[placemark.x, placemark.y]}
-  onContextMenu={()=>deleteMarkerBD(index)}
-/>
+                key={index}
+                geometry={[placemark.x, placemark.y]}
+                onContextMenu={() => deleteMarkerBD(placemark)}
+              />
             ))}
             {newPlacemarksCoordinates.map((coords, index) => (
-              <Placemark id={index} key={index} geometry={coords}  onContextMenu={()=>setDeleteMarkerIndex(index)}/>
-            ))}
-
+  <Placemark id={index} key={index} geometry={coords} onContextMenu={() => startDeleteMarker(index)} />
+))}
             <TypeSelector options={{ float: "right" }} />
           </Map>
         </YMaps>
       </div>
+      <Modal
+        open={open}
+        title="Удаление маркера"
+        onOk={handleOk}
+        okText='Удалить'
+        cancelText='Отмена'
+        okType='danger'
+        onCancel={handleCancel}
+        footer={(_, { OkBtn, CancelBtn }) => (
+          <>
+            <CancelBtn />
+            <OkBtn />
+          </>
+        )}
+      >
+        <p>Вы уверены в своем выборе?</p>
+      </Modal>
     </div>
   );
 };
